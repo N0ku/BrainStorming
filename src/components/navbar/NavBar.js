8115a2baf987032a168c5fc08fa2379c679e6d85
Nakku <lucielecouturier2@gmail.com>
2023-06-25 17:54:20 +0200

    ✨ - #6 Update Home Page

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect,useRef } from "react";
 import { Link } from "react-router-dom";
 
 import "./NavBar.scss";
 
 export function NavBar() {
+
+  const navBackground = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      var viewportHeight = window.innerHeight; 
+      var threshold = viewportHeight * 1;
+
+        if (window.scrollY > threshold) {
+          navBackground.current.classList.add("nav-solid");
+        } else {
+          navBackground.current.classList.remove("nav-solid");
+        }
+    };
+
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
   return (
     <div className="nav-container">
-      <div className="background-nav"></div>
+      <div className="background-nav" ref={navBackground}></div>
       <nav>
         <ul>
           <li>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import Geometry from "../../assets/videos/geometric.mp4";
 import { Link } from "react-router-dom";
 import Arrow from "../../assets/images/browse_arrow.svg";
 import { NavBar } from "../../components/navbar/NavBar";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 import "./Home.scss";
 
@@ -29,54 +30,43 @@ export function Home() {
         </div>
       </header>
       <section>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu dolor
-          hendrerit, consectetur risus et, pharetra neque. Aliquam laoreet
-          hendrerit eros, sit amet sagittis lorem pulvinar et. Nunc dapibus
-          metus eu tempus fringilla. In hac habitasse platea dictumst. Sed
-          scelerisque non leo vitae imperdiet. Fusce egestas sapien ligula, vel
-          dignissim enim ultrices non. Proin egestas eleifend risus, sed
-          consectetur dui hendrerit a. Phasellus pharetra tincidunt lacus eu
-          ultrices. Phasellus dui enim, molestie quis dolor et, convallis
-          sodales lectus. Etiam dictum mauris nec nulla mollis, molestie
-          pellentesque lorem ullamcorper. Mauris mollis elit erat, non tempus
-          urna fermentum a. Vestibulum nec arcu at nibh placerat venenatis.
-          Curabitur feugiat, felis eget facilisis egestas, massa ante suscipit
-          nibh, mattis commodo tortor massa id sapien. Curabitur convallis urna
-          nec odio pulvinar tincidunt. Proin cursus quam magna, nec hendrerit
-          urna hendrerit in. Curabitur scelerisque aliquam augue aliquam
-          faucibus. Aenean et sapien porttitor, blandit erat a, facilisis quam.
-          Aenean quis leo molestie, commodo augue non, semper lorem. Sed vitae
-          metus pretium, blandit diam suscipit, condimentum dolor. Aenean sit
-          amet velit ex. Integer iaculis commodo sem tincidunt sagittis.
-          Maecenas molestie nisl ut arcu gravida sagittis at non metus. Maecenas
-          scelerisque urna ac ligula consequat, non vehicula turpis condimentum.
-          Pellentesque placerat vitae ipsum eget viverra. Curabitur scelerisque
-          maximus blandit. Morbi in tempus orci, sed commodo nunc. Ut blandit
-          egestas tortor nec iaculis. Vestibulum ante ipsum primis in faucibus
-          orci luctus et ultrices posuere cubilia curae; Quisque malesuada metus
-          quis quam dictum gravida. Maecenas sodales ultrices massa et varius.
-          Aenean tempus tempor sapien, sed aliquet felis laoreet ac. Nunc varius
-          mattis risus, sit amet lacinia sapien tincidunt quis. Suspendisse
-          ornare neque id elementum tempor. Donec sit amet pharetra tellus, eu
-          pellentesque nunc. Sed ac dictum lectus, quis commodo enim.
-          Pellentesque non congue lacus. Nunc volutpat urna ac tristique
-          posuere. Vestibulum ut nibh rutrum, fermentum ex a, lobortis purus.
-          Sed consequat nisi scelerisque purus consectetur, ac dapibus metus
-          cursus. Integer pretium rhoncus commodo. Duis pharetra ut augue
-          molestie porttitor. Sed rutrum cursus leo non tincidunt. Mauris sit
-          amet nisi a risus porttitor laoreet. In hac habitasse platea dictumst.
-          Morbi porttitor convallis ultrices. Curabitur lobortis velit id lacus
-          gravida dictum. Cras sagittis purus est, ut tristique magna euismod
-          et. Curabitur rutrum lacus molestie mauris bibendum aliquet. Mauris ut
-          quam nec tortor fermentum mollis. Nulla dictum dui at mollis suscipit.
-          Pellentesque lacinia aliquet sapien non consequat. Morbi eu iaculis
-          magna. Nam bibendum leo rhoncus elit finibus sagittis. Nam dictum
-          vulputate porttitor. Vestibulum ac ullamcorper sapien. Duis finibus
-          mauris nibh, quis hendrerit risus venenatis eu. Suspendisse nulla
-          massa, molestie a tellus vel, ultricies iaculis elit. Donec accumsan
-          est euismod nibh auctor placerat.
-        </p>
+        <div className="gameplay">
+          <div className="titles-gameplay">
+            <h2>Play with React Three Fiber</h2>
+              <p>
+                lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                vitae elit libero, a pharetra augue. Nullam id dolor id nibh
+                ultricies vehicula ut id elit. Cras justo odio, dapibus ac
+                facilisis in, egestas eget quam. Donec id elit non mi porta
+                gravida at eget metus. Nullam quis risus eget urna mollis ornare
+                vel eu leo. Nullam quis risus eget urna mollis ornare vel eu
+                leo. Donec sed odio dui. Nullam quis risus eget urna mollis
+                ornare vel eu leo. Donec sed odio dui. Nullam quis risus eget
+                urna mollis ornare vel eu leo. Donec sed odio dui. Nullam quis
+                risus eget urna
+              </p>
+            <Link to="/gameplay">
+              Discover <ArrowRightAltIcon className="button-icon" />
+            </Link>
+          </div>
+          <div className="image-gameplay">
+            <img
+              alt="gameplay"
+              src="https://i.ytimg.com/vi/1rP3nNY2hTo/maxresdefault.jpg"
+              id="first"
+            ></img>
+            <img
+              alt="gameplay"
+              src={require("../../assets/images/img2.png")}
+              id="second"
+            ></img>
+            <img
+              alt="gameplay"
+              src={require("../../assets/images/img.png")}
+              id="third"
+            ></img>
+          </div>
+        </div>
       </section>
     </>
   );
